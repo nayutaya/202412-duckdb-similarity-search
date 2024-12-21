6426195a1b6aaeb4ac68854afd192e62499f36c5
Yuya Kato <yuyakato@gmail.com>
2024-12-21 14:52:29 +0900

    キーワード引数に変更

--- vector-similarity-search/plot_random_dim3.py
+++ vector-similarity-search/plot_random_dim3.py
@@ -12,7 +12,7 @@ print((features.dtype, features.shape))
 # 散布図を描画する
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(111, projection="3d")
-ax.scatter(features[:, 0], features[:, 1], features[:, 2], s=1, alpha=0.7)
+ax.scatter(xs=features[:, 0], ys=features[:, 1], zs=features[:, 2], s=1, alpha=0.7)
 plt.tight_layout()
 plt.savefig("random_dim3.png", dpi=300, bbox_inches="tight")
 plt.show()
