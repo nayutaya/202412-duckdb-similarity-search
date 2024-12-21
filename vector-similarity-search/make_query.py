73a3437454f20ac9b756854f4e6bc87e80e5fe40
Yuya Kato <yuyakato@gmail.com>
2024-12-21 14:46:11 +0900

    データそのものではなく、データ型、シェイプを出力するように変更

--- vector-similarity-search/make_query.py
+++ vector-similarity-search/make_query.py
@@ -8,7 +8,7 @@ n_dims = int(sys.argv[1])
 
 # ランダムなクエリベクトルを生成する
 query = normalize(np.random.rand(1, n_dims).astype(np.float32) - 0.5)[0]
-print(query)
+print((query.dtype, query.shape))
 
 # クエリベクトルを保存する
 query_file_name = f"query_dim{n_dims}.npy"
